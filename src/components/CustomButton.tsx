15a78625c1d7c77a4aa7a3f2f2966796ad6218fa
Ikhsan PUTRA <ikhsanputra@gmail.com>
2024-08-07 22:06:59 +0200

    add components Hero, CustomButton, Navbar, Footer. add types, constants, images. add tailwind config. add tsconfig.

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { CustomButtonProps as Props } from "@types";
+
+const CustomButton = ({
+  isDisabled,
+  btnType,
+  containerStyles,
+  textStyles,
+  title,
+  rightIcon,
+  handleClick,
+}: Props) => (
+  <button
+    disabled={isDisabled}
+    type={btnType || "button"}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
+  >
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className="relative w-6 h-6">
+        <Image
+          src={rightIcon}
+          alt="arrow_left"
+          fill
+          className="object-contain"
+        />
+      </div>
+    )}
+  </button>
+);
+export default CustomButton;
